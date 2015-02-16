782127cf2dd3124c1f74a28aee84c4c37cbcd683
patrickshampine <patrick@patrickshampine.com>
2015-02-16 13:18:37 -0800

    Slight rewrite, merge into single bash script

---
+++ bin/deploy.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+while getopts "p:b:h:" flag
+  do
+    case $flag in
+      p )
+        path=$OPTARG
+        ;;
+      b )
+        branch=$OPTARG
+        ;;
+      h )
+        hash=$OPTARG
+        ;;
+  esac
+done
+
+if[[ ! -z ${hash+x} ]]; then
+
+  # Hard rest on force push
+  git -C $path fetch --all --tags
+  git -C $path reset --hard $hash
+
+else
+
+  # Manual pull via fetch/merge
+  # Allows working copy to not be the same branch as GitHub
+  # Also makes sure we have an up-to-date local copy if things break
+  git -C $path fetch --all --tags
+  git -C $path merge origin/$branch
+
+fi
--- server.js
+++ server.js
@@ -6,28 +6,35 @@ var config = require('./config.js'),
 console.log('Now listening on port ' + config.port);
 
 gith({ repo: config.repo }).on('all', function(payload) {
-  var command;
 
   // We are only running a deploy on the config branch
   if(config.branch === payload.branch) {
 
     // Handles a force push by doing a git reset --hard COMMIT
     if(true === payload.forced) {
-      exec('./bin/reset.sh', function(error, stdout, stderr) {
-
-      });
-
+      doDeploy(payload.after);
     } else {
-      command = './bin/pull.sh -p ' + config.path + ' -b ' + config.branch;
-      exec(command, function(error, stdout, stderr) {
-
-        console.log('error: ' + error);
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-
-      });
-
+      doDeploy(false);
     }
+  } else {
+    console.log('Push event received on different branch set in config.')
   }
 
 });
+
+
+function doDeploy(hash) {
+  var command;
+  var now  = moment()
+  var time = now.format('YYYY-MM-DD HH:mm:ss');
+
+  command = './bin/deploy.sh -p ' + config.path + ' -b ' + config.branch;
+
+  if(hash !== false) command += ' -h ' + hash;
+
+  exec(command, function(error, stdout, stderr) {
+    console.log('error: ' + error);
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+  });
+}
